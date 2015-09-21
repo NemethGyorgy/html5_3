09c5e8ae37529c06242da6e8e0879e8328c15d42
unknown <html@2-03.itfactory.neta>
2015-09-21 20:04:05 +0200

    animáció

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// elemek kiválasztása.
+// @param selector: string az elemet kiválasztó szelektor.
+function _q( selector ) {
+    
+    // Elemek kiválasztása.
+    var elements = document.querySelectorAll( selector );
+    
+    return elements;
+}
+
+// Profil beviteli mező megjelenítése.
+function showProfile() {
+    
+    //Elem kiválasztása.
+    var div = document.querySelector( ".profile-div" );
+    
+    //Ha a show class hozzá vana dva az elemhez elrejtjük, különben megjelenítjük.
+    if ( div.classList.contains( "show" )) {
+        div.classList.remove( "show" );
+    } else {
+        div.classList.add( "show" );
+    }
+}
